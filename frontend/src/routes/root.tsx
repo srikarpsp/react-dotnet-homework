12ecb7d1f1fedc3838906670f80be44c9d132310
Mark Bennett <mark@burmis.ca>
2023-11-30 22:11:49 +0000

    Ok, header made sense. Adding back.

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -185,6 +185,11 @@ export default function Root() {
               </>
             )}
           </Disclosure>
+           <header className="py-10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-white">Weather</h1>
+            </div>
+          </header>
         </div>
 
         <main className="-mt-32">

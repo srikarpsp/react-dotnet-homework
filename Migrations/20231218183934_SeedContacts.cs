78606cf645ca5b2057db9158b72655382aeb7afc
Mark Bennett <mark@burmis.ca>
2023-12-18 18:41:56 +0000

    Add migration to seed database

---
+++ Migrations/20231218183934_SeedContacts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vscode_remote_try_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedContacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Bob" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Jane" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Mike" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3 });
+        }
+    }
+}
